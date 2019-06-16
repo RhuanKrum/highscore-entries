2fd9bc6c2d3024a08305fb906619e32543a31321
Rhuan Krum <rhuan.krum@hotmail.com>
2019-06-16 01:41:54 -0400

    Add initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO highscore(id, name, score) values (1, 'John', 100);
+INSERT INTO highscore(id, name, score) values (2, 'Ashley', 150);
+INSERT INTO highscore(id, name, score) values (3, 'Washington', 130);
+INSERT INTO highscore(id, name, score) values (4, 'Josh', 95);
+INSERT INTO highscore(id, name, score) values (5, 'Paul', 120);
