7cbb08130a2d905ef15cb991602cb3700d5067b8
Rhuan Krum <rhuan.krum@hotmail.com>
2019-06-16 00:25:08 -0400

    Add Jetty embedded server

---
+++ src/main/java/com/highscores/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.highscores.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.highscores")
+public class AppConfig {
+}
---
+++ src/main/webapp/pages/index.jsp
@@ -0,0 +1,9 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<!DOCTYPE HTML>
+<html>
+    <head></head>
+    <body>
+        <p>Hello!</p>
+    </body>
+</html>
